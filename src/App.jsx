85031fa2736c77f6f6f349eec63fdaf2adc0c6f2
mr.abosi@gmail.com <mr.abosi@gmail.com>
2024-01-30 16:38:59 +0300

    Trying two Call-back functions (One) passes Props from data.js and button selector function as a value allong with onSelect button trigger, and (Two) passes function as a value  and child props

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,11 @@ function App() {
       console.log(selectedButton);
   }
 
+  function handleSelectOne(selectedButton)
+  {
+      console.log(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -26,19 +31,22 @@ function App() {
         </section>
         <section id='examples'>
         <h2>Examples</h2>
+
         <menu>
-
-        <TabButton title={CORE_CONCEPTS[0].title} onSelect={() => handleSelect(CORE_CONCEPTS[0].title)} />
-<TabButton title={CORE_CONCEPTS[1].title} onSelect={() => handleSelect(CORE_CONCEPTS[1].title)} />
-<TabButton title={CORE_CONCEPTS[2].title} onSelect={() => handleSelect(CORE_CONCEPTS[2].title)} />
-<TabButton title={CORE_CONCEPTS[3].title} onSelect={() => handleSelect(CORE_CONCEPTS[3].title)} />
-
-          {/*
+          {/*  Method 1 using TabButton via TabButton  -- Not Appearing*/}
           <TabButton onSelect={() => handleSelect('Components')}>Components</TabButton>
           <TabButton onSelect={() => handleSelect('JSX')}>JSX</TabButton>
           <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
           <TabButton onSelect={() => handleSelect('State')}>State</TabButton>
-          */}
+        </menu>
+
+
+        <menu>
+        {/*  Method 2 using TabButtonProp via TabButton -- Appearing */}
+        <TabButton title={CORE_CONCEPTS[0].title} onSelect={() => handleSelectOne(CORE_CONCEPTS[0].title)} />
+        <TabButton title={CORE_CONCEPTS[1].title} onSelect={() => handleSelectOne(CORE_CONCEPTS[1].title)} />
+        <TabButton title={CORE_CONCEPTS[2].title} onSelect={() => handleSelectOne(CORE_CONCEPTS[2].title)} />
+        <TabButton title={CORE_CONCEPTS[3].title} onSelect={() => handleSelectOne(CORE_CONCEPTS[3].title)} />
         </menu>
         Dynamic Content
         </section>
